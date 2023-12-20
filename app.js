8fcb12407caf46288703c63e2f6f7ed200228416
angwanghyeon <agh1594@naver.com>
2023-12-20 18:35:43 +0900

    231220 안광현

--- app.js
+++ app.js
@@ -4,10 +4,50 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const fs = require('fs');
+
+//JSON 파일 읽기
+let rawdata = fs.readFileSync('connection.json');
+let config = JSON.parse(rawdata);
+
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  host: config.HOST,
+  user : config.USER,
+  password : config.password,
+  database : config.SCHEMA
+});
+
+connection.connect();
+module.exports = connection;
+
+//미들웨어 전부 ok임 -> 전부 말고 특정 주소만
+const corsOptions = {
+  origin : 'http://localhost:3000',
+  methods : 'GET, HEAD, PUT, PATCH,POST,DELETE'
+}
+app.use('/api/data', cors(corsOptions));
+// app.use('/');
+
+app.get('/api/data', (req, res) => {
+  connection.query('select * from detectfiles', (err, rows, fields) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    // rows를 JSON 문자열로 변환하여 클라이언트에게 전달
+    res.send(rows);
+  });
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
