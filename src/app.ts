fbf057b7f6291e3aecfafcd54ec27b2e9e4a3c42
tjnoh <tjnoh@ciotsecurity.com>
2024-01-02 18:41:01 +0900

    240102 18:40 typescript 변환, pieCharts 미완성

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { NextFunction, Request, Response } from "express";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -37,15 +39,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // app.use('/');
 
-app.get('/api/detectfiles', (req, res) => {
-  connection.query('select * from detectfiles', (err, rows, fields) => {
+app.get('/api/detectfiles', (req:Request, res : Response) => {
+  connection.query('select * from detectfiles', (err:Error, rows:Response) => {
     if (err) {
       console.error('Error executing query:', err);
-      return res.status(500).send('Internal Server Error');
+      // return res.status(500).send('Internal Server Error');
+      return res.status(500).json('Internal Server Error');
     }
 
     // rows를 JSON 문자열로 변환하여 클라이언트에게 전달
-    res.send(rows);
+    res.send(rows.json);
   });
 });
 
@@ -65,18 +68,19 @@ app.use('/users', usersRouter);
 app.use('/pie', pieChartRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req:Request, res:Response, next:NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err:Error, req:Request, res:Response, next:NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  if(err.message !== '') res.status(+err.message);
+  else                   res.status(500);
   res.render('error');
 });
 
