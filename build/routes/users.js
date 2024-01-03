e5832c18d78528aeb54f0afd4137b82d6f1ca112
tjnoh <tjnoh@ciotsecurity.com>
2024-01-03 17:45:38 +0900

    240103 1745 back source

--- build/routes/users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require('express');
-var router = express.Router();
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.status(200).send('users');
-});
-module.exports = router;
