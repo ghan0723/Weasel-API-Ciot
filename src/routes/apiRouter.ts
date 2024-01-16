dc3b72078a515f46a4986ee9c135395de8140a0a
tjnoh <tjnoh@ciotsecurity.com>
2024-01-16 15:23:38 +0900

    240116 merge 전

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -19,15 +19,6 @@ router.get("/", (req: Request, res: Response) => {
   const desc = req.query.desc;         // desc : false, asc : true, undefined
   const category = req.query.category; // search column
   const search = req.query.search;     // search context
-  console.log("contents : " , contents);
-  console.log("page : " , page);
-  console.log("pageSize : " , pageSize);
-  console.log("sorting : ", sorting);
-  console.log("desc : ", desc);
-  console.log("category : " , category);
-  console.log("search : " , search);
-  console.log("page type : ", typeof(page));
-  console.log("page type : ", typeof(pageSize));  
   
   let results;
 
@@ -53,5 +44,76 @@ router.get("/", (req: Request, res: Response) => {
       res.status(500).send("server error");
     });
 });
+
+// dummy data 생성
+router.get('/dummy', (req:Request, res:Response) => {
+  const contents = req.query.contents;
+  const count = req.query.count;
+  // const page = req.query.page;         // page count
+  const pageSize = req.query.pageSize; // page 갯수
+  const sorting = req.query.sorting;   // sort column
+  const desc = req.query.desc;         // desc : false, asc : true, undefined
+  const category = req.query.category; // search column
+  const search = req.query.search;     // search context
+  console.log("dummy : ");
+
+  switch(contents) {
+    case 'network':
+      networkService.getDummyData(count)
+      .then(() => {
+        getApiDataLogic(contents,0,pageSize,sorting,desc,category,search,req,res);
+      })
+      .catch((error) => {
+        console.error(error + " : " + contents);
+        res.status(500).send("server error");
+      });
+    break;
+  }
+
+});
+
+// data 다중 삭제
+router.post('/rm', (req:Request, res:Response) => {
+  const contents = req.query.contents;
+  // const page = req.query.page;         // page count
+  const pageSize = req.query.pageSize; // page 갯수
+  const sorting = req.query.sorting;   // sort column
+  const desc = req.query.desc;         // desc : false, asc : true, undefined
+  const category = req.query.category; // search column
+  const search = req.query.search;     // search context
+  const body = req.body;
+
+  switch(contents) {
+    case 'network':
+      networkService.postRemoveData(body)
+      .then(() => {     
+        getApiDataLogic(contents,0,pageSize,sorting,desc,category,search,req,res);
+      })
+      .catch((error) => {
+        console.error(error + " : " + contents);
+        res.status(500).send("server error");
+      });
+
+    break;
+  }
+});
+
+// 송신탐지 외 getApiData Logic
+function getApiDataLogic(contents:any,page:any,pageSize:any,sorting:any,desc:any,category:any,search:any,req:Request,res:Response) {
+  switch(contents) {
+    case 'network' :
+      networkService.getApiData(page,pageSize,sorting,desc,category,search)    
+      .then(result => {          
+        res.send(result);
+      })
+      .catch((error) => {
+        console.error("getApiDataLogic(network) : " + error);
+        res.status(500).send("server error");
+      })
+    break;
+  }
+}
+
+
 
 export = router;
