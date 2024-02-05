273846394c97179daef4a61134bd3f4cb55cc673
tjnoh <tjnoh@ciotsecurity.com>
2024-02-05 16:27:25 +0900

    240205 1630 [태종] merge 전

--- build/interface/ipDomain.js
+++ build/interface/ipDomain.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.backIP = exports.frontIP = void 0;
-exports.frontIP = 'https://172.31.168.112:3000';
-exports.backIP = 'https://172.31.168.112:8000';
+exports.frontIP = 'https://172.31.168.110:3000';
+exports.backIP = 'https://172.31.168.110:8000';
