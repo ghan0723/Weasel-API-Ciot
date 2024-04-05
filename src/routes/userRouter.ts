31e06121baa06f2a29d8b700066962ca417c95bd
angwanghyeon <agh1594@naver.com>
2024-04-05 15:24:50 +0900

    240405 kadif merge ghan

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -58,7 +58,7 @@ router.post("/login", (req: Request, res: Response) => {
                           // weasel.log(username,req.socket.remoteAddress,"로그인에 성공하였습니다 팝업의 내용을 확인해주세요.");
                           res
                             .status(200)
-                            .send({ username, freq: false, notice: true,  });
+                            .send({ username, freq: false, notice: true, priv:result[0].privilege});
                         } else {
                           //팝업이 존재하지 않는다면
                           res.cookie("username", user[0].username, {
@@ -70,7 +70,7 @@ router.post("/login", (req: Request, res: Response) => {
                           // weasel.log(username,req.socket.remoteAddress,"로그인에 성공하였습니다.");
                           res
                             .status(200)
-                            .send({ username, freq: false, notice: false,  });
+                            .send({ username, freq: false, notice: false, priv:result[0].privilege });
                         }
                       })
                       .catch((error5) => {
@@ -252,7 +252,7 @@ router.post("/add", (req: Request, res: Response) => {
                   //대역을 넘지 않을 때
                   //freq 값 추가
                   userService
-                    .getFreq(user.cookie)
+                    .getFreq()
                     .then((result) => {
                       userService
                         .addUser(newUser, result[0].pwd_change_freq)
@@ -297,7 +297,7 @@ router.post("/add", (req: Request, res: Response) => {
           } else {
             //관리자 계정 freq
             userService
-              .getFreq(user.cookie)
+              .getFreq()
               .then((result) => {
                 userService
                   .addUser(newUser, result[0].pwd_change_freq)
@@ -724,6 +724,41 @@ router.post("/pwd", (req: Request, res: Response) => {
       // weasel.error(username,req.socket.remoteAddress,"변경 전 비밀번호를 데이터베이스에 조회하는 쿼리 실행 중 오류가 발생했습니다.");
       res.status(500).send("error :" + error2);
     });
+});
+
+router.post('/sign-up', (req:Request, res:Response) => {
+  let user = req.body;
+  let encPasswd = cryptoService.getEncryptUltra(user.passwd)
+  userService.checkUsername(user.username)
+  .then((resultDup) => {
+    if(resultDup.exists){
+      res.status(401).send({ error: '입력한 사용자명이 중복되어 회원가입에 실패하였습니다.' });
+    } else {
+      userService.getFreq()
+      .then((freq) => {
+        const newUser = {
+          username:user.username,
+          passwd:encPasswd,
+          privilege: user.privilege,
+          ip_ranges: user.ip_ranges,
+          freq:freq[0].pwd_change_freq,
+        }
+        userService.signUp(newUser)
+        .then((result) => {
+          res.send(result);
+        })
+        .catch((error) => {
+          res.status(500).send("error :" + error);
+        })
+      })
+      .catch((errorFreq) => {
+        res.status(500).send("error :" + errorFreq);
+      })
+    }
+  })
+  .catch((errorDup) => {
+    res.status(500).send("error :" + errorDup);
+  })
 });
 
 export = router;
